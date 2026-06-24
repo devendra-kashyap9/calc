79ee7f220db2d208aa0e313df688e461bcf00574
devendra-kashyap9 <dkashyap099@gmail.com>
2026-06-24 20:41:43 +0530

    Initial Commit adding calc.py

---
+++ calc.py
@@ -0,0 +1,9 @@
+# This is a program to add 2 Numbers
+
+def add(a: float, b:float) -> float:
+    return a + b
+
+a, b = [float(x) for x in input('Enter two numbers to perform addition seperated by ",": ').split(',')]
+sum = add(a, b)
+
+print(f'The resultatnt sum of {a} & {b} is {sum}')
